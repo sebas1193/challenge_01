cfcebabca562e1e48886921de0f49d9c89af2df5
sebas1193 <juansebasitan05@gmail.com>
2026-03-07 16:50:55 -0500

    :recycle: refactor(service-worker): update caching strategy and simplify service worker implementation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,40 +1,61 @@
-const CACHE_NAME = "react-pwa-v2";
+/* CACHE FIRST Cuando se usa esta estrategia es porque 
+queremos primero consultar datos que estén almacenados en el dispositivo que en la red. 
+Entiendo que es cuando algo no cambia recurrentemente. En este caso, como es para algo de gestión de usuarios, 
+pues algo que no es crítico en el sistema de salud a menos que sea para laboratorios u otras cosas como cirugias. */
 
-const APP_SHELL = ["/", "/index.html", "/manifest.json", "/img1.jpg"];
+const CACHE_NAME = "medicare-pwa-v1";
 
+// Archivos del "app shell": los recursos mínimos para que la app se vea y funcione
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+];
+
+// El evento "install" se dispara cuando el service worker se instala por primera vez
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    // crear cahe
+    caches.open(CACHE_NAME).then((cache) => {
+      // guardar cache
+      return cache.addAll(APP_SHELL);
+    })
   );
 });
+
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches
-      .keys()
-      .then((keys) =>
-        Promise.all(
-          keys
-            .filter((key) => key !== CACHE_NAME)
-            .map((key) => caches.delete(key))
-        )
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
       )
+    )
   );
 });
 
-// Network First
 self.addEventListener("fetch", (event) => {
+  // peticiones
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const clone = response.clone();
+    // Primero buscamos en el caché
+    caches.match(event.request).then((respuestaEnCache) => {
+      if (respuestaEnCache) {
+        return respuestaEnCache;
+      }
+
+      // Si no está en caché, entonces red
+      return fetch(event.request).then((respuestaDeRed) => {
+        const copiaParaCache = respuestaDeRed.clone();
         caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, clone);
+          cache.put(event.request, copiaParaCache);
         });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+        // retorna lo encontrado
+        return respuestaDeRed;
+      });
+    })
   );
 });
